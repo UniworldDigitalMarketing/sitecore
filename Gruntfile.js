9753fe574cbaef0eaea28c18cd2552f34d89055d
TurboTodd2k <toddkidder@All-Powerful-Overbrain.local>
2016-06-25 09:06:32 -0700

    Grunt update
    updated grunt to latest version and added a new command, run “grunt
    clean” to get an extracted, prefixed and minified css sheet

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,17 +9,40 @@
                             'dist/cleaned-css/tidy.css': ['dev/index.html']
                         }
                     }
-                }
+                },
+
+
+
+                postcss: {
+                    options: {
+                      map: true, // inline sourcemaps
+
+                     
+
+                      processors: [
+                        
+                        require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                        require('cssnano')() // minify the result
+                      ]
+                    },
+                    dist: {
+                      src: 'dist/cleaned-css/tidy.css'
+                    }
+                  }
+
+
            
         });
 
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-uncss');
+    grunt.loadNpmTasks('grunt-postcss');
 
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['uncss']);
+        grunt.registerTask('clean', ['uncss', 'postcss']);
     
 
     };
